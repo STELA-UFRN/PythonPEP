24580256839a67c4399dc20f37b7b53ac6bd40c3
I-am-Gabi <gabicavalcantesilva@gmail.com>
2016-08-10 12:54:12 -0300

    Organizing files and adding initial basic authentication

---
+++ 2-BasicAuthentication/securityapp-ui/web/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from config import BaseConfig
+import requests
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+
+url_service = "http://192.168.99.100:8000/"
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route("/hello", methods=['GET'])
+def hello(): 
+	r = requests.get(url_service + "service1")
+	return render_template('index.html', content=r.text)
+
+@app.route("/username", methods=['GET'])
+def username(): 
+	r = requests.get(url_service + "service1/" + request.args.get('username')) 
+	return render_template('index.html', content=r.text)
+
+@app.route("/list", methods=['GET'])
+def list(): 
+	r = requests.get(url_service + "service2/list")
+	return render_template('index.html', content=r.text)
+
+@app.route("/add", methods=['GET'])
+def add(): 
+	r = requests.get(url_service + "service2/add/" + request.args.get('name'))
+	return render_template('index.html', content=r.text)
+
+@app.route('/reset')
+def reset():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
